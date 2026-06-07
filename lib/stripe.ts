2dc45c24c2be113e4d7e640b5a0b30784b27fb1b
Vitor <vitor@learningenglishbr.com>
2026-06-07 17:45:30 -0300

    Fix stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10', // Versão mais recente e estável do momento
+  apiVersion: '2026-05-27.dahlia', // Versão exigida pelo SDK instalado
   appInfo: {
     name: 'English School Premium SaaS',
     version: '1.0.0',
